321e60ca081419d6a99ee33e605b7f0021a7adc1
BYUNG <45321943+ilovebyung@users.noreply.github.com>
2024-11-01 15:34:27 +0900

    Update detect_ncnn.py

--- detect_ncnn.py
+++ detect_ncnn.py
@@ -35,28 +35,28 @@ while True:
             try:
 
                 if size == 'l' or size == 'm':
-                    # sound_arg = (size,seconds,)
-                    # t1 = threading.Thread(target=alarm.sound_alarm, args=sound_arg)
-                    alarm.sound_alarm(size, seconds)
+                    sound_arg = (size,seconds,)
+                    t1 = threading.Thread(target=alarm.sound_alarm, args=sound_arg)
+                    # alarm.sound_alarm(size, seconds)
 
                     if side == 'L':
-                        light_arg = (alarm.LR,0.1)
+                        light_arg = (alarm.LR,0.1,)
                         t2 = threading.Thread(target=alarm.turn_on_led, args=light_arg)
                         # alarm.turn_on_led(alarm.LR,0.1)
                     else:
-                        light_arg = (alarm.RR,0.1)
+                        light_arg = (alarm.RR,0.1,)
                         t2 = threading.Thread(target=alarm.turn_on_led, args=light_arg)
                         # alarm.turn_on_led(alarm.RR,0.1)
                 else:
                     alarm.turn_off_led()
 
                 # Start the threads
-                # t1.start()
+                t1.start()
                 t2.start()
 
                 # Wait for both threads to finish   
 
-                # t1.join()
+                t1.join()
                 t2.join()
                 
             except Exception as e:
